1e136581499e3f13bc21974b963efc1225a4049b
DwieTwarze2000 <48262070+DwieTwarze2000@users.noreply.github.com>
2022-12-13 20:19:11 +0100

    added method handler (maintenace_done, reset_error_status, emergency_stop)

--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,15 @@
-from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.device import IoTHubDeviceClient, Message, MethodRequest, MethodResponse
 import json
 from device import Device
+from datetime import datetime
+import asyncio
 class Agent:
   def __init__(self, device, connection_string):
     self.device = device
     self.client = IoTHubDeviceClient.create_from_connection_string(connection_string)
     self.client.connect()
+    self.client.on_method_request_received = self.methods
+    self.tasks = []
     print("Agent created for device: " + self.device.name)
   
   async def telemetry_send(self):
@@ -16,9 +20,43 @@ class Agent:
       "BadCount":await self.device.read("BadCount"),
       "Temperature":  await self.device.read("Temperature")
     }
+    print("Sending...")
     print(sendData)
-    
-    msg = Message(json.dumps(sendData))
+    msg = Message(json.dumps(sendData), "UTF-8", "JSON")
     self.client.send_message(msg)
+  
+  def methods(self, method):
+    """
+    emergency_stop
+    reset_error_status
+    maintenance_done
+    """
+    print("Method called: " + method.name)
+    if method.name == "maintenance_done":
+      print("maintenance done")
+      self.client.patch_twin_reported_properties({"LastMaintenanceDate":  datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
     
- 
+    elif method.name == "reset_error_status":
+      node = self.device.client.get_node(self.device.id)
+      self.tasks.append(node.call_method("ResetErrorStatus"))
+  
+    elif method.name == "emergency_stop":
+      node = self.device.client.get_node(self.device.id)
+      self.tasks.append(node.call_method("EmergencyStop"))
+      print("taski",self.tasks)
+    
+    self.client.send_method_response(MethodResponse(method.request_id, 0))
+  
+
+  def close(self):
+    self.client.shutdown()
+
+  def taskPackgageF(self):
+    taskPackgage = []
+    for task in self.tasks:
+      taskPackgage.append(asyncio.create_task(task))
+    taskPackgage.append(asyncio.create_task(self.telemetry_send()))
+    self.tasks = []
+    return taskPackgage
+
+    
--- src/main.py
+++ src/main.py
@@ -4,22 +4,39 @@ from factory import Factory
 from agent import Agent
 from device import Device
 from configwriter import Config
-# from configparser import ConfigParser
+import time
 
 async def test():
-  config = Config()
-  async with Client(url="opc.tcp://localhost:4840/") as client:
-    factory = Factory(client)
-    factory = await factory.create(client)
-    await factory.get_device_names()
+  try:
+    config = Config()
+    async with Client(url="opc.tcp://localhost:4840/") as client:
+      factory = Factory(client)
+      factory = await factory.create(client)
+      await factory.get_device_names()
+      agents = []
+      for device in factory.devices:
+        connection_str = config.get_connection_string(device.name)
+        agent = Agent(device, connection_str)
+        await agent.telemetry_send()
+        agents.append(agent)
+      try:
+        while True:
+          tasks = []
+          for agent in agents:
+            agent_tasks = agent.taskPackgageF()
+            for task in agent_tasks:
+              tasks.append(task)
+          await asyncio.gather(*tasks)
+          time.sleep(5)
+      except Exception as e:
+        print(e) 
+        for agent in agents:
+          agent.close()
+  except KeyboardInterrupt:
+    print("Goodbye! ;)")
+    for agent in agents:
+      agent.close()
 
-    agents = []
-    for device in factory.devices:
-      connection_str = config.get_connection_string(device.name)
-      agent = Agent(device, connection_str)
-      await agent.telemetry_send()
-      agents.append(agent)
-    print(agents)
-  
 if __name__ == "__main__":
-  asyncio.run(test())
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(test())
